81a37757993c0ccc566d8472606e36cc89ec9cf1
yashas3013 <katteyashas@gmail.com>
2024-05-01 11:15:10 +0530

    fixed null cell while adding obstacles

--- build/dstar_lite_planner/CMakeFiles/dstar_lite_planner_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for dstar_lite_planner_uninstall.
--- src/planner.cpp
+++ src/planner.cpp
@@ -1,162 +1,171 @@
 #include <chrono>
+#include <cmath>
 #include <functional>
 #include <memory>
 #include <vector>
-#include <cmath>
 
-#include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/path.hpp"
+#include "Dstar.h"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
 #include "nav_msgs/msg/odometry.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "Dstar.h"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
+class PathPlanner : public rclcpp::Node {
+public:
+  PathPlanner() : Node("path_planner") {
+    path_publisher = this->create_publisher<nav_msgs::msg::Path>("path", 10);
+    map_subscription = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
+        "grid_msg", 10, std::bind(&PathPlanner::map_callback, this, _1));
+    goal_subscription =
+        this->create_subscription<geometry_msgs::msg::PoseStamped>(
+            "goal_pose", 10, std::bind(&PathPlanner::goal_callback, this, _1));
+    pose_subscription = this->create_subscription<nav_msgs::msg::Odometry>(
+        "odom", 10, std::bind(&PathPlanner::pose_callback, this, _1));
+    timer_ = this->create_wall_timer(
+        30ms, std::bind(&PathPlanner::timer_callback, this));
+  }
 
-class PathPlanner : public rclcpp::Node
-{
-  public:
-    PathPlanner() : Node("path_planner")
-    {
-        path_publisher = this->create_publisher<nav_msgs::msg::Path>("path", 10);
-        map_subscription = this->create_subscription<nav_msgs::msg::OccupancyGrid>("map", 10, std::bind(&PathPlanner::map_callback, this, _1));
-        goal_subscription=this->create_subscription<geometry_msgs::msg::PoseStamped>("goal_pose",10,std::bind(&PathPlanner::goal_callback,this,_1));
-        pose_subscription=this->create_subscription<nav_msgs::msg::Odometry>("odom",10,std::bind(&PathPlanner::pose_callback,this,_1));
-        timer_ = this->create_wall_timer(30ms, std::bind(&PathPlanner::timer_callback, this));
+private:
+  Dstar dstar;
+
+  nav_msgs::msg::Path path;
+  list<state> mypath;
+  std::vector<geometry_msgs::msg::PoseStamped> pose_array;
+  geometry_msgs::msg::PoseStamped path_points;
+
+  int start_row, start_col;
+  int map_width, map_height;
+  float resolution = 0.1;
+  std::vector<int8_t> map_data;
+
+  float yaw;
+
+  int threshold = 80;
+  bool once = false;
+
+  bool flag_0 = false;
+  bool flag_1 = false;
+  int pose_update = 10;
+
+  float start_x, start_y;
+
+  float goal_x, goal_y = 0;
+
+  void timer_callback() {
+    if (this->once == false) {
+      this->dstar.init(0, 0, 0, 0);
+      this->once = true;
     }
 
-  private:
+    if (this->flag_0 == true) {
+      if (this->flag_1 == true) {
+        this->dstar.updateStart(this->start_x / this->resolution,
+                                this->start_y / this->resolution);
+        this->dstar.replan();
 
-    Dstar dstar;
+        this->pose_array.clear();
 
-    nav_msgs::msg::Path path;
-    list<state> mypath;
-    std::vector<geometry_msgs::msg::PoseStamped> pose_array;
-    geometry_msgs::msg::PoseStamped path_points;
-    
-    int start_row, start_col;
-    int map_width, map_height;
-    float resolution=0.1;
-    std::vector<int8_t> map_data;
+        this->mypath = this->dstar.getPath();
 
-    float yaw;
-
-    int threshold=80;
-    bool once=false;
-
-    bool flag_0=false;
-    bool flag_1=false;
-    int pose_update=10;
-
-    float start_x, start_y;
-
-    float goal_x, goal_y=0;
-    
-
-    void timer_callback()
-    {   
-        if(this->once==false){
-            this->dstar.init(0,0,0,0);
-            this->once=true;}
-
-        if(this->flag_0==true){
-            if(this->flag_1==true){
-                this->dstar.updateStart(this->start_x / this->resolution, this->start_y / this->resolution);
-                this->dstar.replan();
-
-                this->pose_array.clear();
-
-                this->mypath = this->dstar.getPath();
-
-                for (state path_state : this->mypath){
-                    this->path_points.pose.position.x=path_state.x * this->resolution;
-                    this->path_points.pose.position.y=path_state.y * this->resolution;
-                    this->pose_array.push_back(this->path_points);
-                }
-                
-                RCLCPP_INFO(this->get_logger(), "Path updated");  
-                }
+        for (state path_state : this->mypath) {
+          this->path_points.pose.position.x = path_state.x * this->resolution;
+          this->path_points.pose.position.y = path_state.y * this->resolution;
+          this->pose_array.push_back(this->path_points);
         }
 
-        this->path.header.frame_id="odom";
-        this->path.poses = this->pose_array;
-        path_publisher->publish(this->path);
+        RCLCPP_INFO(this->get_logger(), "Path updated");
+      }
     }
 
-    void map_callback(const nav_msgs::msg::OccupancyGrid::SharedPtr map)
-    {   
-        RCLCPP_INFO(this->get_logger(), "Map generated"); 
-        
-        this->resolution=map->info.resolution;
+    this->path.header.frame_id = "odom";
+    this->path.poses = this->pose_array;
+    path_publisher->publish(this->path);
+  }
 
-        this->start_col=(map->info.origin.position.x / this->resolution);
-        this->start_row=(map->info.origin.position.y / this->resolution);
-            
-        this->map_width = map->info.width;
-        this->map_height = map->info.height;
-        this->map_data=map->data;
+  void map_callback(const nav_msgs::msg::OccupancyGrid::SharedPtr map) {
+    RCLCPP_INFO(this->get_logger(), "Map generated");
 
-        int count=0;
+    this->resolution = map->info.resolution;
 
-        this->flag_1=false;
-               
-        for(int i=0; i<this->map_height; i++){
-            for(int j=0; j<this->map_width; j++){
+    this->start_col = (map->info.origin.position.x / this->resolution);
+    this->start_row = (map->info.origin.position.y / this->resolution);
 
-                // if(this->map_data[count]>this->threshold){
+    this->map_width = map->info.width;
+    this->map_height = map->info.height;
+    this->map_data = map->data;
 
-                //             this->dstar.updateCell(j+this->start_col,  i+this->start_row, -1);
-                //     }
-                if(this->map_data[count]==-1) ;
-                else this->dstar.updateCell(j+this->start_col,  i+this->start_row, this->map_data[count]);//+sqrt(pow((this->goal_x-this->start_x) / this->resolution,2)+pow((this->goal_y-this->start_y) / this->resolution,2)));
+    int count = 0;
 
-                count++;
-            }
+    this->flag_1 = false;
+
+    for (int i = 0; i < this->map_height; i++) {
+      for (int j = 0; j < this->map_width; j++) {
+
+        // if(this->map_data[count]>this->threshold){
+
+        //             this->dstar.updateCell(j+this->start_col,
+        //             i+this->start_row, -1);
+        //     }17
+        RCLCPP_INFO(this->get_logger(), "%i", this->map_data[count]);
+
+        if (this->map_data[count] == -1 || this->map_data[count] == 0)
+          ;
+        else {
+          // RCLCPP_INFO(this->get_logger(), "%i", this->map_data[count]);
+          this->dstar.updateCell(j + this->start_col, i + this->start_row,
+                                 this->map_data[count]);
+          // sqrt(pow((this->goal_x - this->start_x) / this->resolution,
+          //          2) +
+          //      pow((this->goal_y - this->start_y) / this->resolution,
+          //          2)));
         }
-   
-        RCLCPP_INFO(this->get_logger(), "Map updated"); 
-        this->flag_1=true; 
-    
+        count++;
+      }
     }
 
+    RCLCPP_INFO(this->get_logger(), "Map updated");
+    this->flag_1 = true;
+  }
 
-    void goal_callback(const geometry_msgs::msg::PoseStamped::SharedPtr goal){
+  void goal_callback(const geometry_msgs::msg::PoseStamped::SharedPtr goal) {
 
-        RCLCPP_INFO(this->get_logger(), "Goal received");
+    RCLCPP_INFO(this->get_logger(), "Goal received");
 
-        this->goal_x=goal->pose.position.x;
-        this->goal_y=goal->pose.position.y;
-        this->dstar.updateGoal(this->goal_x / this->resolution, this->goal_y / this->resolution);  
+    this->goal_x = goal->pose.position.x;
+    this->goal_y = goal->pose.position.y;
+    this->dstar.updateGoal(this->goal_x / this->resolution,
+                           this->goal_y / this->resolution);
+  }
 
-    }
+  void pose_callback(const nav_msgs::msg::Odometry::SharedPtr pose) {
 
-    void pose_callback(const nav_msgs::msg::Odometry::SharedPtr pose){
-        
-        this->start_x=pose->pose.pose.position.x;
-        this->start_y=pose->pose.pose.position.y;
-        // double siny_cosp = 2 * (pose.pose.orientation.w * pose.pose.orientation.z + pose.pose.orientation.x * pose.pose.orientation.y);
-        // double cosy_cosp = 1 - 2 * (pose.pose.orientation.y* pose.pose.orientation.y+ pose.pose.orientation.z * pose.pose.orientation.z);
-        // this->yaw = std::atan2(siny_cosp, cosy_cosp);
-        this->flag_0=true;
-        
-    }
+    this->start_x = pose->pose.pose.position.x;
+    this->start_y = pose->pose.pose.position.y;
+    // double siny_cosp = 2 * (pose.pose.orientation.w * pose.pose.orientation.z
+    // + pose.pose.orientation.x * pose.pose.orientation.y); double cosy_cosp =
+    // 1 - 2 * (pose.pose.orientation.y* pose.pose.orientation.y+
+    // pose.pose.orientation.z * pose.pose.orientation.z); this->yaw =
+    // std::atan2(siny_cosp, cosy_cosp);
+    this->flag_0 = true;
+  }
 
-
-    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_subscription;
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_subscription;
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr pose_subscription;
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher;
-    
+  rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr
+      map_subscription;
+  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr
+      goal_subscription;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr pose_subscription;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher;
 };
 
-int main(int argc, char * argv[])
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PathPlanner>());
-    rclcpp::shutdown();
-    return 0;
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PathPlanner>());
+  rclcpp::shutdown();
+  return 0;
 }
